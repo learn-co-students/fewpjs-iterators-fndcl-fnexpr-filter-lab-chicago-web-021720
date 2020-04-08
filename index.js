eda9c8f3f3b4f43608056fbbe9614dd30e6e54d7
Ethan Davidson <theejdavidson@gmail.com>
2020-04-07 21:55:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    const matches = drivers.filter(s => {
+        return s.toLowerCase() === str.toLowerCase()
+    });
+    return matches;
+}
+
+function fuzzyMatch(drivers, str) {
+    const matches = drivers.filter(s => {
+        return s.toLowerCase().charAt(0) === str.toLowerCase().charAt(0);
+    });
+    return matches;
+}
+
+function matchName(drivers, str) {
+    const matches = drivers.filter(s => {
+        return s['name'] === str;
+    });
+    return matches;
+}
